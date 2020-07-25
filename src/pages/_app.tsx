91be04a6f784260930e8f0033ddaa50f29050bc8
Johny W. Alves <johnywalves@gmail.com>
2020-07-25 11:27:33 -0300

    :wrench: Add PWA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,9 +10,10 @@ function App({ Component, pageProps }: AppProps) {
         <title>React Avançado - Boilerplate</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
-          content="A simple project start to work with TypeScript, React, NextJS and Styled Components"
+          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
         />
       </Head>
       <GlobalStyles />
