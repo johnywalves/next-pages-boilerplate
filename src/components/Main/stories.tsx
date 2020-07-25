40e8c1bb894814db588863734dfbb4b50df4fb35
Johny W. Alves <johnywalves@gmail.com>
2020-07-25 09:54:37 -0300

    :wrench: Add Storybook

---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,19 @@
+import { withKnobs, text } from '@storybook/addon-knobs'
+
+import Main from '.'
+
+export default {
+  title: 'Main',
+  component: Main,
+  decorators: [withKnobs]
+}
+
+export const Basic = () => (
+  <Main
+    title={text('Title', 'React Avançado')}
+    description={text(
+      'Description',
+      'TYpeScript, ReactJS, NextJS e Styled Components'
+    )}
+  />
+)
