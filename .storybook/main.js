1e3916b1d2023ec79808c92c9c2a141a14a59840
Johny W. Alves <johnywalves@gmail.com>
2022-01-25 13:50:15 -0300

    :arrow_up: Upgrade dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,9 @@
 module.exports = {
   stories: ['../src/components/**/*stories.tsx'],
-  addons: ['@storybook/addon-essentials'],
-  babel: async (options) => ({
-    ...options,
-    plugins: [
-      ...options.plugins,
-      require.resolve('@babel/plugin-transform-react-jsx')
-    ]
-  }),
+  addons: ['@storybook/addon-essentials', 'storybook-addon-next-router'],
+  core: {
+    builder: 'webpack5'
+  },
   webpackFinal: (config) => {
     config.resolve.modules.push(`${process.cwd()}/src`)
     return config
