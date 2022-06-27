55011ca6d9924c6c9028c42b2efe1087bd5ff041
Johny W. Alves <johnywalves@gmail.com>
2022-06-27 15:36:21 -0300

    :wrench: Adicionar metadatas para páginas

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,18 +2,42 @@ import { AppProps } from 'next/app'
 import Head from 'next/head'
 import GlobalStyles from 'styles/global'
 
+import manifest from '../../public/manifest.json'
+
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        <meta name="theme-color" content={manifest.theme_color} />
+
+        <title>{manifest.short_name}</title>
+        <meta name="title" content={manifest.short_name} />
+        <meta name="description" content={manifest.description} />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={manifest.public_url} />
+        <meta property="og:site_name" content={manifest.name} />
+        <meta property="og:title" content={manifest.short_name} />
+        <meta property="og:description" content={manifest.description} />
+        <meta
+          property="og:image"
+          itemProp="image"
+          content={manifest.thumbnail}
+        />
+        <meta property="og:locale" content="pt_BR" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content={manifest.public_url} />
+        <meta property="twitter:title" content={manifest.name} />
+        <meta property="twitter:description" content={manifest.description} />
+        <meta property="twitter:image" content={manifest.thumbnail} />
+
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
-        <meta
-          name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
-        />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
