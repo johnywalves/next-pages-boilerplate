5f2b07cec30b548c5419587dee3875aef0eab054
Johny W. Alves <johnywalves@gmail.com>
2022-10-13 19:09:25 -0300

    :arrow_up: Upgrade dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../src/components/**/*stories.tsx'],
+  stories: ['../src/components/**/*stories.tsx', '../src/views/**/*stories.tsx'],
   addons: ['@storybook/addon-essentials', 'storybook-addon-next-router'],
   core: {
     builder: 'webpack5'
