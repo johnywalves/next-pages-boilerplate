ee433ee48d1ee9452dacc3e96b7a1556427e3e21
Johny W. Alves <johnywalves@gmail.com>
2022-10-25 18:05:49 -0300

    :wrench: Configuration to Next.js 13

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withPWA = require('next-pwa')
 const isProd = process.env.NODE_ENV === 'production'
 
 const config = {
-  experimental: { appDir: true, forceSwcTransforms: true }
+  experimental: { forceSwcTransforms: true }
 }
 
 if (isProd) {
