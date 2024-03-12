4a4a1debfb84ad3b1865db2e0c625cc2bc6ca221
Johny W. Alves <johnywalves@gmail.com>
2024-03-12 11:36:38 -0300

    :wrench: configurar cobertura de testes

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,12 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/*stories.tsx'],
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/pages/**',
+    '!src/types/**',
+    '!src/**/*stories.tsx'
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   moduleDirectories: ['node_modules', 'src'],
   moduleNameMapper: {
